f7450a0c1c936796e96196bb4675085372079da2
gagansingh894 <gds31.gagandeepsingh@gmail.com>
2021-10-21 02:44:55 +0530

    added clean_text function

--- utils/helper_functions.py
+++ utils/helper_functions.py
@@ -1,3 +1,9 @@
+import re
+from nltk.corpus import stopwords
+
+my_stopwords = list(stopwords.words('english'))
+
+
 def process_text(inp: str, replace_whitespace: bool = False, to_lower: bool = False) -> str:
     """
     This function removes unwanted punctuation from the text and converts to proper text, lower text based on flag.
@@ -22,6 +28,18 @@ def check_for_digits(inp: str) -> bool:
     """
     inp.replace(" ", "")
     return any([char.isdigit() for char in inp])
+
+
+def clean_text(inp: str) -> str:
+    """
+    Clean text by removing special symbols and stopwords
+    :param inp: input string
+    :return: cleaned string
+    """
+    inp = ' '.join([w.lower() for w in inp.split(" ") if w.lower() not in my_stopwords])
+    inp = re.sub('[^A-Za-z0-9]+', '', inp)
+    return inp
+
 
 if __name__ == "__main__":
     pass
