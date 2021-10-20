d661de2e94d8107500da89e278af41c298a0e64e
gagansingh894 <gds31.gagandeepsingh@gmail.com>
2021-10-21 00:36:54 +0530

    added config file

---
+++ utils/config.py
@@ -0,0 +1,3 @@
+MODEL_PATH = "resources/stanford-ner/classifiers/english.all.3class.distsim.crf.ser.gz"
+CLASS_PATH = "resources/stanford-ner/stanford-ner.jar"
+PRODUCT_TOKEN_PATH = "resources/product_tokens"
